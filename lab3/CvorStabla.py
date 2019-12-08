ffbe74cf765bde22331ea12865aa870abe35d8f5
Dorotea <dorotea.franjic@fer.hr>
2019-12-08 17:28:29 +0100

    Dodane funkcije stringToString za cvor stabla, za ispis liste djece i liste tipova.

--- lab3/CvorStabla.py
+++ lab3/CvorStabla.py
@@ -6,10 +6,24 @@ class CvorStabla:
         self.podaci = podaci
         self.dubina = dubina
 
+    def __repr__(self):
+        #samo za ispis vrijednosti i dubine
+        stringToString = "Dubina ovog čvora: " + str(self.dubina) + " " + "Podaci ovog čvora: " + str(self.podaci) + "\n"
+        return stringToString
+
     def dodaj_dijete(self, cvor):
         if cvor is None:
             return
         cvor.dubina = self.dubina + 1
         self.lista_djece.append(cvor)
 
+    def prikaziDjecu(self):
+        for dijete in self.lista_djece:
+            print(dijete, end=" ")
+        return
+
+    def prikaziTipove(self):
+        for tip in self.lista_tipova:
+            print(tip, end=" ")
+        return
     #def ispis_podstabla(self):
