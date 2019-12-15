089a320037822b1ba0da4181a52245a1619dc7ce
PBubica <pb50564@fer.hr>
2019-12-15 20:26:59 +0100

    Stvoren novi modul PomocneFunkcije koji sadrzi bas ono sto mislis da sadrzi.

---
+++ lab3/PomocneFunkcije.py
@@ -0,0 +1,43 @@
+import config
+
+def izracunaj_duljinu_znakova(cvor_stabla):
+    return
+
+def ide_u_niz_znakova(cvor_stabla):
+    return
+
+def provjeri_tipove(cvor_stabla_1, cvor_stabla_2):
+    return
+
+def vrati_lokalnu_deklaraciju(ime):
+    return
+
+def je_deklarirano_lokalno(ime):
+    return
+
+def je_vec_deklarirano(ime):
+    return
+
+def funkcija_vec_postoji(cvor_tablice, ime_funkcije, tip_funkcije):
+    return
+
+def konfliktna_deklaracija(cvor_tablice, ime_funkcije, tip_funkcije):
+    return
+
+def je_castable(tip_1, tip_2):
+    return
+
+def ispisi_error_poruku(cvor_stabla):
+    return
+
+def je_integer(x):
+    return
+
+def je_char(x):
+    return
+
+def je_string(x):
+    return
+
+def vrati_tip_trenutne_funkcije():
+    return
